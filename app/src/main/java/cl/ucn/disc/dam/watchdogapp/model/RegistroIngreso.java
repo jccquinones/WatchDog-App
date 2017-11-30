22d1b00b61220dc7bd6d5992a3cfb72afa6eed19
John <jqm004@alumnos.ucn.cl>
2017-11-30 16:38:57 -0300

    Added: Registro de ingreso class to model

---
+++ app/src/main/java/cl/ucn/disc/dam/watchdogapp/model/RegistroIngreso.java
@@ -0,0 +1,15 @@
+package cl.ucn.disc.dam.watchdogapp.model;
+
+import java.util.Date;
+
+/**
+ * Created by JOHN on 30-11-2017.
+ */
+
+public class RegistroIngreso {
+
+    private String porteria;
+
+    private Date fecha;
+
+}
