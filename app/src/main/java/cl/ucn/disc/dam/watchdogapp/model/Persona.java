49c6d1b6d2d07f138547bd674d2abe9e3cb87947
John <jqm004@alumnos.ucn.cl>
2017-11-30 16:35:55 -0300

    Added: Persona to model

---
+++ app/src/main/java/cl/ucn/disc/dam/watchdogapp/model/Persona.java
@@ -0,0 +1,19 @@
+package cl.ucn.disc.dam.watchdogapp.model;
+
+/**
+ * Created by JOHN on 30-11-2017.
+ */
+
+public class Persona {
+
+    private int rut;
+
+    private String nombre;
+
+    private String correoElectronico;
+
+    private int telefono;
+
+    private int numeroAnexo;
+
+}
---
+++ app/src/main/java/cl/ucn/disc/dam/watchdogapp/model/Vehiculo.java
@@ -0,0 +1,8 @@
+package cl.ucn.disc.dam.watchdogapp.model;
+
+/**
+ * Created by JOHN on 30-11-2017.
+ */
+
+public class Vehiculo {
+}
