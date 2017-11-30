3c481596bee586ec3812e6bb447d9f232596c03f
John <jqm004@alumnos.ucn.cl>
2017-11-30 17:06:10 -0300

    Added : AppDatabase to Dao

---
+++ app/src/main/java/cl/ucn/disc/dam/watchdogapp/dao/AppDatabase.java
@@ -0,0 +1,17 @@
+package cl.ucn.disc.dam.watchdogapp.dao;
+
+
+import com.raizlabs.android.dbflow.annotation.Database;
+
+/**
+ * @author JOHN
+ */
+@Database(version = AppDatabase.VERSION)
+public final class AppDatabase {
+
+    /**
+     * The Version
+     */
+    public static final int VERSION = 1;
+
+}
